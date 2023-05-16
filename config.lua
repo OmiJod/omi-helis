f8d9de28453881b2e311620b5cbf1b228a4e2539
Omi <omchavan9400@gmail.com>
2023-05-16 12:28:11 -0700

    Added more plaers

--- config.lua
+++ config.lua
@@ -4,5 +4,6 @@ Config.Debug = false
 
 Config.Table = {
     ['ZWN20218'] = 'volatus', --- Citizen ID and Helicopter Name
-    ['PGV03008'] = 'frogger', --- Citizen ID and Helicopter Name
+    ['HAP75810'] = 'volatus', --- Citizen ID and Helicopter Name
+    ['VVZ33308'] = 'volatus', --- Citizen ID and Helicopter Name
 }
